a88476c22734790f6dfa70b1ea5a48e61c39d55d
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-03-08 22:15:53 +0400

    expense

---
+++ app/src/main/java/com/andef/myfinance/data/database/expense/mapper/ExpenseModelListToExpenseListMapper.kt
@@ -0,0 +1,22 @@
+package com.andef.myfinance.data.database.expense.mapper
+
+import com.andef.myfinance.data.database.expense.model.ExpenseModel
+import com.andef.myfinance.domain.database.expense.entities.Expense
+
+class ExpenseModelListToExpenseListMapper {
+    private fun map(expenseModel: ExpenseModel): Expense {
+        return Expense(
+            id = expenseModel.id,
+            amount = expenseModel.amount,
+            category = expenseModel.category,
+            comment = expenseModel.comment,
+            date = expenseModel.date
+        )
+    }
+
+    fun map(expenseModelList: List<ExpenseModel>): List<Expense> {
+        return expenseModelList.map {
+            this.map(it)
+        }
+    }
+}
---
+++ app/src/main/java/com/andef/myfinance/data/database/expense/repository/ExpenseRepositoryImpl.kt
@@ -0,0 +1,54 @@
+package com.andef.myfinance.data.database.expense.repository
+
+import com.andef.myfinance.data.database.expense.dao.ExpenseDao
+import com.andef.myfinance.data.database.expense.mapper.ExpenseModelListToExpenseListMapper
+import com.andef.myfinance.data.database.expense.mapper.ExpenseToExpenseModelMapper
+import com.andef.myfinance.domain.database.expense.entities.Expense
+import com.andef.myfinance.domain.database.expense.entities.ExpenseCategory
+import com.andef.myfinance.domain.database.expense.repository.ExpenseRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.util.Date
+import javax.inject.Inject
+
+class ExpenseRepositoryImpl @Inject constructor(
+    private val dao: ExpenseDao,
+    private val expenseToExpenseModelMapper: ExpenseToExpenseModelMapper,
+    private val expenseModelListToExpenseListMapper: ExpenseModelListToExpenseListMapper
+) : ExpenseRepository {
+    override suspend fun addExpense(expense: Expense) {
+        dao.addExpense(expenseToExpenseModelMapper.map(expense))
+    }
+
+    override suspend fun changeExpense(
+        expense: Expense,
+        newAmount: Double?,
+        newCategory: ExpenseCategory?,
+        newComment: String?,
+        newDate: Date?
+    ) {
+        dao.changeExpense(
+            id = expense.id,
+            newAmount = newAmount ?: expense.amount,
+            newCategory = newCategory ?: expense.category,
+            newComment = newComment ?: expense.comment,
+            newDate = newDate ?: expense.date
+        )
+    }
+
+    override suspend fun removeExpense(id: Int) {
+        dao.removeExpense(id)
+    }
+
+    override fun getExpense(date: Date): Flow<List<Expense>> {
+        return dao.getExpense(date.time).map {
+            expenseModelListToExpenseListMapper.map(it)
+        }
+    }
+
+    override fun getExpense(startDate: Date, endDate: Date): Flow<List<Expense>> {
+        return dao.getExpense(startDate.time, endDate.time).map {
+            expenseModelListToExpenseListMapper.map(it)
+        }
+    }
+}
