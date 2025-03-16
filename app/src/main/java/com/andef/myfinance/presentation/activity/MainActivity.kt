29950527e2ff468c18774257631ecbbbc9d340d9
Дмитрий Семкин <semkin_dmitriy10@vk.com>
2025-03-16 19:36:29 +0400

    Add web view

--- app/src/main/java/com/andef/myfinance/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/andef/myfinance/presentation/activity/MainActivity.kt
@@ -8,13 +8,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Snackbar
 import androidx.compose.material3.SnackbarDuration
